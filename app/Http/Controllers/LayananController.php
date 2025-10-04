d506461a2f7c2b033282eb634b3237adcfad9e3d
taufik464 <ridotuhan7@gmail.com>
2025-10-04 14:00:38 +0700

    update 4-10-25

--- app/Http/Controllers/LayananController.php
+++ app/Http/Controllers/LayananController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 use App\Models\kontak;
 
 use App\Models\Prestasi;
+use App\Models\IdentitasSekolah;
 use Illuminate\Http\Request;
 
 class LayananController extends Controller
 {
     public function kontak()
     {
+        $identitas = IdentitasSekolah::first();
          $kontaks = kontak::all();
-        return view('layanan.kontak', compact('kontaks'));
+        return view('layanan.kontak', compact('kontaks', 'identitas'));
     }
 
     public function prestasiSekolah()
--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -43,10 +43,10 @@
         </div>
 
         <!-- Kolom Kanan: Gambar -->
-        <div class="flex justify-center relative">
-            <img src="/img/Selfi.jpg" alt="Santri Berjajar" class="rounded-lg shadow-lg w-full md:w-4/5 relative z-10">
+        <div class="flex justify-center relative overflow-hidden">
+            <img src="/img/OIP.png" alt="Santri Berjajar" class="rounded-lg  w-full md:w-4/5 relative z-10 transform translate-x-full transition-transform duration-1000 ease-out"
+                onload="this.classList.remove('translate-x-full')">
             <!-- Efek dekorasi lingkaran blur -->
-            <div class="absolute -top-6 -right-6 w-40 h-40 bg-green-300/30 rounded-full blur-2xl z-0"></div>
         </div>
     </div>
 </section>
@@ -80,13 +80,9 @@
                     player.pauseVideo();
                 }
             });
-<<<<<<< HEAD
-        }, { threshold: 0.5 });
-=======
         }, {
             threshold: 0.5
         });
->>>>>>> 055395bf1346ec6bd7f815984d689799ea24666d
 
         observer.observe(document.getElementById('yt-video'));
     }
@@ -137,36 +133,36 @@
 
 <!-- Berita Terkini -->
 <section class="py-16 bg-gray-100" id="berita">
-    <div class="max-w-7xl mx-auto px-6">
-        <h2 class="text-3xl font-bold text-center text-gray-800 mb-10">Berita Terkini</h2>
+        <div class="max-w-7xl mx-auto px-6  ">
+            <h2 class="text-3xl font-bold text-center text-gray-800 mb-10">Berita Terkini</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                <div class="bg-white rounded-xl shadow p-6">
+                    <img src="/img/Juara.jpg" alt="Berita 1" class="mb-4 rounded-lg w-full object-cover h-40">
+                    <h3 class="text-lg font-semibold text-gray-800 mb-2">Juara Lomba</h3>
+                    <p class="text-sm text-gray-600">Siswa kelas 12 Juara.</p>
+                </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            <div class="bg-white rounded-xl shadow p-6">
-                <img src="/img/Juara.jpg" alt="Berita 1" class="mb-4 rounded-lg w-full object-cover h-40">
-                <h3 class="text-lg font-semibold text-gray-800 mb-2">Juara Lomba</h3>
-                <p class="text-sm text-gray-600">Siswa kelas 12 Juara.</p>
+                <div class="bg-white rounded-xl shadow p-6">
+                    <img src="/img/Juara.jpg" alt="Berita 2" class="mb-4 rounded-lg w-full object-cover h-40">
+                    <h3 class="text-lg font-semibold text-gray-800 mb-2">Juara 1 Lomba Tahfidz</h3>
+                    <p class="text-sm text-gray-600">Santri SMA Al Hikmah berhasil meraih juara dalam lomba tahfidz tingkat kabupaten.</p>
+                </div>
+
+                <div class="bg-white rounded-xl shadow p-6">
+                    <img src="/img/Juara.jpg" alt="Berita 3" class="mb-4 rounded-lg w-full object-cover h-40">
+                    <h3 class="text-lg font-semibold text-gray-800 mb-2">Juara Lomba</h3>
+                    <p class="text-sm text-gray-600">Kelas 11 berhasil mendapatkan juara 2.</p>
+                </div>
             </div>
 
-            <div class="bg-white rounded-xl shadow p-6">
-                <img src="/img/Juara.jpg" alt="Berita 2" class="mb-4 rounded-lg w-full object-cover h-40">
-                <h3 class="text-lg font-semibold text-gray-800 mb-2">Juara 1 Lomba Tahfidz</h3>
-                <p class="text-sm text-gray-600">Santri SMA Al Hikmah berhasil meraih juara dalam lomba tahfidz tingkat kabupaten.</p>
-            </div>
-
-            <div class="bg-white rounded-xl shadow p-6">
-                <img src="/img/Juara.jpg" alt="Berita 3" class="mb-4 rounded-lg w-full object-cover h-40">
-                <h3 class="text-lg font-semibold text-gray-800 mb-2">Juara Lomba</h3>
-                <p class="text-sm text-gray-600">Kelas 11 berhasil mendapatkan juara 2.</p>
+            <!-- Tombol Lihat Semua Berita -->
+            <div class="text-center mt-8">
+                <a href="{{ route('berita') }}"
+                    class="inline-block bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-lg font-medium transition">
+                    Lihat Semua Berita
+                </a>
             </div>
         </div>
-
-        <!-- Tombol Lihat Semua Berita -->
-        <div class="text-center mt-8">
-            <a href="{{ route('berita') }}"
-                class="inline-block bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-lg font-medium transition">
-                Lihat Semua Berita
-            </a>
-        </div>
-    </div>
+   
 </section>
 @endsection
